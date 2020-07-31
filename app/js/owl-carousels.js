55232792985c970bb1a651eff0236c0f1e6129b1
rupeshubhagat <bhagat.r29@gmail.com>
2020-08-01 00:27:12 +0530

    working on about us

--- app/js/owl-carousels.js
+++ app/js/owl-carousels.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //Video Carousel.. homepage
     $('#video-carousel').owlCarousel({
         loop: true,
         margin: 38,
@@ -19,6 +20,33 @@ $(document).ready(function() {
             },
             1000: {
                 items: 2,
+                nav: false,
+                loop: true
+            }
+        }
+    })
+
+    //Our-team Carousel About us page
+    $('#team-carousel').owlCarousel({
+        loop: true,
+        margin: 121,
+        responsiveClass: true,
+        // autoWidth: true,
+        // autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                margin: 10
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
                 nav: false,
                 loop: true
             }
